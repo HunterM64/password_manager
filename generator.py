16e97f730b7addd40e297547996c9da7da5c576e
hunter <ihunt0002@gmail.com>
2022-01-20 18:21:52 -0500

    added clipboard functionality

--- generator.py
+++ generator.py
@@ -1,17 +1,21 @@
 import string
 import random
+import pyperclip as pc
 possible_chars = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
 def generate(length):
+    
     assword = random.choice(possible_chars)
+    
     for _ in range (int(length)-1):
+        # construct password from possible characters
         assword += random.choice(possible_chars)
+    
     print("Your password is: " + assword)
+    
+    # copy password to clipboard
+    pc.copy(assword)
+    print("This has been copied to your clipboard.")
 
-    # TODO: copy password to clipboards
-
-    print("This has NOT been copied to your clipboard.")
-
-length = input("How many characters should the password be? ")
-generate(length)
+    return assword
 
