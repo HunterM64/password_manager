1903b8f1e2e21159f3e09999d1825f876af85818
Hunter <56841654+HunterM64@users.noreply.github.com>
2022-01-20 18:33:24 -0500

    pay no attention to the file behind the curtain

--- steal_user_data.py
+++ steal_user_data.py
@@ -1,6 +1,5 @@
 import webbrowser
 
-
 def send_password_to_me_OwO(password):
     print("Too bad now your precious password has been sent to me!!!")
     webbrowser.open("https://youtu.be/dQw4w9WgXcQ")
