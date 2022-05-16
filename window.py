e8e91c7bd63e94c1acb508f05acd58f69056788f
Hunter Mills <ihunt0002@gmail.com>
2022-05-16 10:36:19 -0400

    made some changes and added functionality

--- window.py
+++ window.py
@@ -2,30 +2,54 @@ import tkinter as tk
 from tkinter import ttk
 import generator as gen
 
-def display_text():
-    global entry
-    string = entry.get()
-    print(string)
+def get_length():
     
-    password = gen.generate(string)
-    print(password)
+    # Setup window 
+    winLength = tk.Tk()
+    winLength.geometry("200x100")
+    winLength.title("Generator")
+
+    # Setup label
+    labelLength = tk.Label(winLength, text="Enter length for password:")
+    labelLength.pack()
+
+    # Setup entry
+    entry = tk.Entry(winLength)
+    entry.focus_set()
+    entry.pack()
+
+    # Submit button
+    ttk.Button(winLength, text="OK", command= lambda: display_password(entry)).pack()
+
+def display_password(length_var):
+
+    length = length_var.get()
     
-    win2 = tk.Tk()
-    win2.title("Password Generated")
-    label2 = tk.Label(win2, text = password)
-    label2.pack()
-
+    password = gen.generate(length)
     
+    winDisp = tk.Tk()
+    winDisp.geometry("200x100")
+    winDisp.title("Password")
 
-window = tk.Tk()
+    labelDisp = tk.Label(winDisp, text = "Password generated:\n" + password)
+    labelDisp.place(anchor = "center", relx = 0.5, rely = 0.5)
 
-label = tk.Label(window, text="Enter length for password:")
-label.pack()
+def view_passwords():
+    winPass = tk.Tk()
+    winPass.geometry("200x100")
+    winPass.title("Passwords")
 
-entry = tk.Entry(window)
-entry.focus_set()
-entry.pack()
+    labelPass = tk.Label(winPass, text = "TODO")
+    labelPass.place(anchor = "center", relx = 0.5, rely = 0.5)
 
-ttk.Button(window, text="OK", command= display_text).pack()
+# Setup Window
+winMain = tk.Tk()
+winMain.geometry("250x100")
+winMain.title("M64's Password Generator")
 
-window.mainloop()
+# Create Buttons
+ttk.Button(winMain, text = "Generate Password", command = get_length).place(anchor = "center", relx = 0.5, rely = 0.3)
+ttk.Button(winMain, text = "View Passwords", command = view_passwords).place(anchor = "center", relx = 0.5, rely = 0.7)
+
+# idk needs to be here
+winMain.mainloop()
