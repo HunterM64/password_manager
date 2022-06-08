e99178da4faeea67d7fcf68254a1b9eed1a12554
Hunter Mills <ihunt0002@gmail.com>
2022-06-08 09:44:54 -0400

    changed functionality of some functions

--- manager.py
+++ manager.py
@@ -6,46 +6,52 @@ import os
 def savePassword(website, password):
     # securely save password
     file = open(".super_secure_password_file", "a")
-    file.write("Website: " + website + "\tPassword: " + password + "\n")
+    file.write(website + "\t" + password + "\n")
     file.close()
     
 def readPasswords():
-    # dump all passwords to terminal
-    file = open(".super_secure_password_file", "r")
-    print(file.read())
-    # TODO - make this better / have more functionality
+    passwordDict = {}
+    with open(".super_secure_password_file") as file:
+        for line in file:
+            website, password = line.split("\t")
+            if website not in passwordDict:
+                passwordDict[website] = password
+            else:
+                passwordDict.update({website: password})
+    return passwordDict
 
-os.system('clear' if os.name =='posix' else 'cls')
-print("M64 Password Management System ----------")
-print("1. Generate new password")
-print("2. View passwords")
-print("3. Exit program")
-choice = input(": ")
 
-while True:
+# os.system('clear' if os.name =='posix' else 'cls')
+# print("M64 Password Management System ----------")
+# print("1. Generate new password")
+# print("2. View passwords")
+# print("3. Exit program")
+# choice = input(": ")
 
-    if choice == '1':
-        length = input("How many characters should the password be? ")
-        if int(length) < 4:
-            print("How about you come back when you have a real request?")
-            break
-        password = generate(length)
+# while True:
 
-        choice = input("Would you like to save this password? (Y/N): ")
-        if choice == 'y' or choice == 'Y':
-            name = input("What website is this password for? ")
-            savePassword(name, password)
-        break
+#     if choice == '1':
+#         length = input("How many characters should the password be? ")
+#         if int(length) < 4:
+#             print("How about you come back when you have a real request?")
+#             break
+#         password = generate(length)
 
-    elif choice == '2':
-        readPasswords()
-        choice = input(": ")
+#         choice = input("Would you like to save this password? (Y/N): ")
+#         if choice == 'y' or choice == 'Y':
+#             name = input("What website is this password for? ")
+#             savePassword(name, password)
+#         break
 
-    elif choice == '3':
-        print("goodbye!")
-        break
-        # this is done perfectly well done
+#     elif choice == '2':
+#         readPasswords()
+#         choice = input(": ")
 
-    else: 
-        print("incorrect option")
-        choice = input(": ")
+#     elif choice == '3':
+#         print("goodbye!")
+#         break
+#         # this is done perfectly well done
+
+#     else: 
+#         print("incorrect option")
+#         choice = input(": ")
